09cd6f7452c2d69cab55815b034be29639afc9b1
L0L-kek-31415 <tanyaboiko1209@gmail.com>
2022-11-16 19:25:56 +0300

    v.1 docker

--- parser/siteservice.py
+++ parser/siteservice.py
@@ -1,37 +1,33 @@
 import multiprocessing
 import time
-from selenium import webdriver
-from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 from parser.class_names import ClassNames
+from parser.driver import DriverService
 from parser.worker import Worker
 
 
-class SiteService:
+class SiteService(DriverService):
     def __init__(self, pages, workers, link):
+        super().__init__()
         self.pages = pages
         self.workers = workers
         self.link = link
         self.manager = multiprocessing.Manager()
         self.queue = self.manager.Queue()
         self.pool = multiprocessing.Pool(self.workers)
-        self.driver_location = "./chromedriver"
-        self.driver = webdriver.Chrome(executable_path=self.driver_location)
 
     def start(self):
         self.driver.get(self.link)
-
-        self.load_posts()
-        self.driver.close()
+        self.posts()
+        self.close_driver()
         self.pool.close()
         self.pool.join()
         while self.queue.empty() is False:
             print(self.queue.get())
 
-    def load_posts(self):
-        counter = self.pages
-        while counter > 0:
+    def posts(self):
+        while self.pages > 0:
             script = "window.scrollTo(0, document.body.scrollHeight);"
             self.driver.execute_script(script)
             time.sleep(1)
@@ -39,16 +35,25 @@ class SiteService:
                 By.CLASS_NAME,
                 ClassNames.POST,
             )
-            for post in new_posts:
-                if counter <= 0:
-                    break
-                try:
-                    link = post.find_element(
-                        By.CLASS_NAME, ClassNames.LINK
-                    ).get_attribute("href")
-                except NoSuchElementException:
-                    print("pass")
-                else:
-                    counter -= 1
-                    worker = Worker(link, self.queue)
-                    self.pool.apply_async(worker.start())
+            self.post_to_worker(new_posts)
+
+    def post_to_worker(self, new_posts):
+        for post in new_posts:
+            if self.pages <= 0:
+                break
+            link = self.get_post_link(post)
+            if link:
+                self.pages -= 1
+                worker = Worker(link, self.queue)
+                self.pool.apply_async(worker.start())
+
+    def get_post_link(self, post):
+        link = self.get_attr(
+            By.CLASS_NAME,
+            ClassNames.LINK,
+            text=True,
+            driver=post,
+        )
+        if link:
+            link = link.get_attribute("href")
+        return link
--- parser/worker.py
+++ parser/worker.py
@@ -1,25 +1,24 @@
 import time
-from selenium import webdriver
-from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
 from parser.class_names import ClassNames
+from parser.driver import DriverService
 
 
-class Worker:
+class Worker(DriverService):
     def __init__(self, link, queue):
+        super().__init__()
         self.link = link
         self.queue = queue
-        self.driver_location = "./chromedriver"
-        self.driver = webdriver.Chrome(executable_path=self.driver_location)
         self.action = ActionChains(self.driver)
 
     def start(self):
+        print("\nstart worker\n")
         self.driver.get(self.link)
         data = self.get_data()
         self.queue.put(data)
-        self.driver.close()
+        self.close_driver()
 
     def get_data(self):
         title = self.get_attr(By.CLASS_NAME, ClassNames.TITLE)
@@ -46,13 +45,3 @@ class Worker:
         time.sleep(0.5)
         time_my = self.get_attr(By.CLASS_NAME, ClassNames.TIME_ALL)
         return time_my
-
-    def get_attr(self, by, value, text=False):
-        try:
-            result = self.driver.find_element(by, value)
-            if text:
-                return result
-            else:
-                return result.text
-        except NoSuchElementException:
-            return None
