d98e14d6e98d543a3f496ecf55dcd43438c151ea
L0L-kek-31415 <tanyaboiko1209@gmail.com>
2022-11-21 22:41:53 +0300

    v.1 update base_class for db

--- parser/db/base_db_class.py
+++ parser/db/base_db_class.py
@@ -3,6 +3,14 @@ from abc import ABC, abstractmethod
 
 class BaseDBService(ABC):
     @abstractmethod
+    def __enter__(self):
+        pass
+
+    @abstractmethod
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    @abstractmethod
     def add_post(self, post):
         pass
 
